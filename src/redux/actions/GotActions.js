1a2fb9acbcf1075e15d1ea8aecf9a3435fd55e66
hitaishlokesh <36701679+hitaishlokesh@users.noreply.github.com>
2021-11-01 00:21:21 +0530

    Update GotActions.js

--- src/redux/actions/GotActions.js
+++ src/redux/actions/GotActions.js
@@ -6,17 +6,3 @@ export const setProducts = (products) => {
     payload: products,
   };
 };
-
-export const selectedProduct = (product) => {
-  return {
-    type: ActionTypes.SELECTED_PRODUCT,
-    payload: product,
-  };
-};
-
-
-export const removeSelectedProduct = () => {
-  return {
-    type: ActionTypes.REMOVE_SELECTED_PRODUCT,
-  };
-};
